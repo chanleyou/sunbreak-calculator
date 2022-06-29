884c2050bf90df41d2332d7ccd85886cd780bb1a
chanleyou <chanleyou@gmail.com>
2022-06-29 23:20:52 +0800

    add sharpness mouseover

--- components/SharpnessBar.tsx
+++ components/SharpnessBar.tsx
@@ -11,17 +11,19 @@ const SharpnessBar = ({ sharpnessArray, small }: Props) => {
 	return (
 		<div className={small ? "" : "py-1 mb-1"}>
 			{!small && <label>Sharpness</label>}
-			<div
-				className={`flex ${small ? "" : "bg-gray-300 border-4 border-r-8 border-gray-300 w-fit"}`}
-			>
+			<div className={`flex ${small ? "" : "bg-gray-300 border-4 border-gray-300 w-fit"}`}>
 				{sharpnessArray.map((v, i) => {
 					if (v === 0) return;
 					return (
 						<div
-							className={`inline-block ${small ? "h-3" : "h-4"}`}
+							className={`group relative inline-block ${small ? "h-3" : "h-4"}`}
 							key={`${id}-${i}`}
 							style={{ width: `${small ? v / 5 : v / 2}px`, backgroundColor: Sharpnesses[i] }}
-						/>
+						>
+							<p className="hidden group-hover:block absolute text-xs p-1 z-50 min-w-max top-6 border border-gray-600 shadow bg-white">
+								{Sharpnesses[i]}: {v}
+							</p>
+						</div>
 					);
 				})}
 			</div>
